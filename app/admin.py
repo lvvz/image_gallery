a7ada35b29d5a4d124406228bfef829e2f594e46
Oleksii Vovchok <36041509+alexvov169@users.noreply.github.com>
2021-05-04 12:43:21 +0300

    Models registered for administration

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Album, Image
 
-# Register your models here.
+
+@admin.register(Album)
+class AlbumModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Image)
+class AlbumImageModelAdmin(admin.ModelAdmin):
+    pass
