bcc1d64a1951312c39cdc372fc8c06d48bcd6c62
Oleksii Vovchok <36041509+alexvov169@users.noreply.github.com>
2021-05-04 13:28:31 +0300

    URL routing to app/ demo

--- app/views.py
+++ app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from .models import Album, Image
 
-# Create your views here.
+
+# TODO: refactor to class
+def visible_albums_list(request):
+    return render(request,
+                  'app/album/list.html',
+                  {
+                      'albums': Album.visible.all()
+                  }
+                  )
+
+
+class AlbumDetail(DetailView):
+    model = Album
+
+    def get_context_data(self, **kwargs):
+        context = super(AlbumDetail, self).get_context_data(**kwargs)
+        context['images'] = Image.objects.filter(album=self.object.id)
+        return context
