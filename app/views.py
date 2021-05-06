c8b8538cfcf5301632016fb10542a6a8279abd75
Oleksii Vovchok <36041509+alexvov169@users.noreply.github.com>
2021-05-06 20:30:23 +0300

    Album list view fix

--- app/views.py
+++ app/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from .models import Album, Image
 
 
-# TODO: refactor to class
-def visible_albums_list(request):
-    return render(
-        request,
-        'album/list.html',
-        {
-            'albums': Album.visible.all()
-        }
-    )
+class AlbumsList(ListView):
+    model = Album
+    context_object_name = 'albums'
+    paginate_by = 3
+
+
+# class VisibleAlbumsList(ListView):
+#     queryset = Album.visible.all()
+#     context_object_name = 'visible_albums'
+#     paginate_by = 3
+#     template_name = 'app/visible_albums_list.html'
 
 
 class AlbumDetail(DetailView):
